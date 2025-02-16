39174dc3c3597b9c1fcfa91dec4d365d4a463d52
unknown <jamie.g.ii@hotmail.com>
2025-02-15 18:59:10 -0600

    Initial setup of raylib and go, made main.go for all work

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
+
+func main() {
+	fmt.Println("Hello World")
+	rl.InitWindow(800, 400, "raylib MicroAssign8 - window")
+}
